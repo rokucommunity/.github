b68d33b0c1e5f6e9d6f478ba22b7467250dd8d21
Christian Holbrook <cholbrook@fubo.tv>
2025-03-06 13:23:03 -0700

    Try to use a different command to get the first commmit

--- src/ChangeLogGenerator.ts
+++ src/ChangeLogGenerator.ts
@@ -49,8 +49,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
         let lastTag = this.getLastTag(project.dir);
         if (!lastTag) {
-            logger.log('No tags found. Creating initial tag v0.0.0. on first commit');
-            const firstCommit = utils.executeCommandWithOutput('git log --reverse --pretty=format:"%H"').toString().trim();
+            logger.log('No tags found. Creating initial tag v0.0.0 on first commit');
+            const firstCommit = utils.executeCommandWithOutput('git rev-list --max-parents=0 HEAD').toString().trim();
             utils.executeCommand(`git tag v0.0.0 ${firstCommit}`, { cwd: project.dir });
             utils.executeCommand(`git push origin v0.0.0`, { cwd: project.dir });
             lastTag = this.getLastTag(project.dir);
